75471bfaf6aec1fec1ddffd5f9cf26578a411ac8
Carl van Schaik <quic_cvanscha@quicinc.com>
2023-06-17 22:29:40 +1000

    charset: convert scripts to utf-8
    Signed-off-by: Carl van Schaik <quic_cvanscha@quicinc.com>

--- scripts/utils/build-llvm.sh
+++ scripts/utils/build-llvm.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# © 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+# Â© 2022 Qualcomm Innovation Center, Inc. All rights reserved.
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
